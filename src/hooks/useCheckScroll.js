3ba10e506f4ff6816216e3751c6ecc8ff8c17522
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-12-28 11:00:40 +0200

    navigation list

--- src/hooks/useCheckScroll.js
+++ src/hooks/useCheckScroll.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 
 export default function useCheckScroll(initialScroll) {
-  const [showMenu, setShowMenu] = useState(initialScroll);
+  const [pageScrollTop, isPageScrollTop] = useState(initialScroll);
   const getScrollData = () => {
-    if (window.scrollY > 50) setShowMenu(false);
-    else setShowMenu(true);
+    if (window.scrollY > 50) isPageScrollTop(false);
+    else isPageScrollTop(true);
   };
 
   useEffect(() => {
     document.addEventListener("scroll", getScrollData);
   }, []);
-  return { showMenu, setShowMenu };
+  return { pageScrollTop, isPageScrollTop };
 }
