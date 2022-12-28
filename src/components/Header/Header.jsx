1ffcd0c935b304e27626cf1117e3546d652f5fd3
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-12-28 22:51:27 +0200

    added tags to menu and fixed hover and hamburger button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import { scrollToId } from "../../utils";
 
 import classes from "./Header.module.css";
 const sourceCode = `</>`;
+const openTag = `< `;
+const closingTag = ` />`;
 const Header = () => {
   return (
     <header id="header">
@@ -17,16 +19,27 @@ const Header = () => {
             href="/"
             onClick={(event) => scrollToId(event, "techStackSection")}
           >
+            <div>{openTag} </div>
             <span>Stack</span>
+            <div>{closingTag}</div>
           </a>
           <a href="/" onClick={(event) => scrollToId(event, "AboutMe")}>
+            <div>{openTag} </div>
+
             <span>About</span>
+            <div>{closingTag}</div>
           </a>
           <a href="/" onClick={(event) => scrollToId(event, "projects")}>
+            <div>{openTag} </div>
+
             <span>Projects</span>
+            <div>{closingTag}</div>
           </a>
           <a href="/" onClick={(event) => scrollToId(event, "footer")}>
+            <div>{openTag} </div>
+
             <span>Contact</span>
+            <div>{closingTag}</div>
           </a>
         </div>
       </div>
